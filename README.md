68fdd1a6a3f617d4dca1b3127f5be57d2fe8b7a6
Vitaly <tihonovcore@gmail.com>
2019-03-06 20:09:28 +0300

    Fix problems

--- README.md
+++ README.md
@@ -5,14 +5,11 @@
 
 ## HW4 Implementor
 ###### Герератор реализации классов и интерфейсов<br>
-[Easy](https://github.com/tihonovcore/Java-advanced/blob/master/src/ru/ifmo/rain/tihonov/implementor/Implementor.java "Простая версия") <br>
-[Hard]( "Усложненная версия") /* там же */
-
+[Solution](https://github.com/tihonovcore/Java-advanced/blob/master/src/ru/ifmo/rain/tihonov/implementor/Implementor.java "Сложная версия")
 
 ## HW3 Student
 ###### Реализация интерфейса базы данных<br>
-[Easy](https://github.com/tihonovcore/Java-advanced/blob/master/src/ru/ifmo/rain/tihonov/student/StudentDB.java "Простая версия") <br>
-[Hard]( "Усложненная версия") /* там же */
+[Solution](https://github.com/tihonovcore/Java-advanced/blob/master/src/ru/ifmo/rain/tihonov/student/StudentDB.java "Простая версия") <br>
 
 Интерфейс простой версии [StudentQuery](https://www.kgeorgiy.info/git/geo/java-advanced-2019/src/master/modules/info.kgeorgiy.java.advanced.student/info/kgeorgiy/java/advanced/student/StudentQuery.java "Простая версия") <br>
 Интерфейс усложненной версии [StudentGroupQuery](https://www.kgeorgiy.info/git/geo/java-advanced-2019/src/master/modules/info.kgeorgiy.java.advanced.student/info/kgeorgiy/java/advanced/student/StudentGroupQuery.java "Усложненная версия")
--- src/ru/ifmo/rain/tihonov/implementor/Implementor.java
+++ src/ru/ifmo/rain/tihonov/implementor/Implementor.java
@@ -6,35 +6,43 @@ import info.kgeorgiy.java.advanced.implementor.ImplerException;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Executable;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Implementor implements Impler {
-    private StringBuilder result = new StringBuilder();
-    private HashSet<Class<?>> imports = new HashSet<>();
-    private Set<Executable> methodSet;
+    private boolean containsNonPrivateConstructor = false;
+    private StringBuilder result;
+    private Set<Executable> methodsSet;
+    private Set<String> methodsTitle;
     private String newLine = System.lineSeparator();
-
+    private String tab = "    ";
 
     @Override
     public void implement(Class<?> token, Path path) throws ImplerException {
         result = new StringBuilder();
-        imports = new HashSet<>();
+        methodsSet = new HashSet<>();
+        methodsTitle = new HashSet<>();
 
-        //todo add class
-        //todo add excep
-        if (token == null || token.equals(void.class) || token.equals(Enum.class)) {
-            throw new ImplerException();
+        if (token == null || path == null) {
+            throw new ImplerException("Arguments can't be null");
         }
 
-        path = path.resolve(token.getPackageName().
-                replace('.', File.separatorChar)).
-                resolve(token.getSimpleName() + "Impl.java");
+        //todo not all
+        if (token.equals(void.class) || token.equals(Enum.class) || Modifier.isFinal(token.getModifiers())) {
+            throw new ImplerException("We can't extends from Enum or void");
+        }
+
+        try {
+            path = path.resolve(token.getPackageName().
+                    replace('.', File.separatorChar)).
+                    resolve(token.getSimpleName() + "Impl.java");
+        } catch (InvalidPathException e) {
+            throw new ImplerException("Path is invalid" + e.getMessage());
+        }
 
         if (path.getParent() != null) {
             try {
@@ -44,49 +52,34 @@ public class Implementor implements Impler {
             }
         }
 
-        methodSet = new TreeSet<>((o1, o2) -> {
-            StringBuilder name1 = new StringBuilder(Arrays.toString(o1.getParameterTypes()));
-            name1.append(o1.getName());
-            StringBuilder name2 = new StringBuilder(Arrays.toString(o2.getParameterTypes()));
-            name2.append(o2.getName());
-            return name1.compareTo(name2);
-        });
-
-        imports.add(token);
-
         findMethods(token);
+        methodsSet.addAll(Arrays.asList(token.getDeclaredConstructors()));
+
         setMethods(token);
         result.append("}");
 
         StringBuilder temp = new StringBuilder();
-        setPackageAndImports(token.getPackageName(), temp);
+        setPackage(token.getPackageName(), temp);
         setTitle(token, temp);
+
+        //todo
+        if (!containsNonPrivateConstructor && !token.isInterface()) {
+            throw new ImplerException("");
+        }
 
         print(temp, path);
     }
 
-    private void setPackageAndImports(String name, StringBuilder temp) {
-        if (!name.equals("")) {
+    private void setPackage(String name, StringBuilder temp) {
+        if (!name.isEmpty()) {
             temp.append("package ").append(name).append(";").append(newLine).append(newLine);
-        }
-
-        for (Class<?> c : imports) {
-            if (!c.isPrimitive() && !c.isArray()) {
-                temp.append("import ").append(c.getPackageName()).append(".").
-                        append(c.getSimpleName()).append(";").append(newLine);
-            }
-        }
-
-        if (!imports.isEmpty()) {
-            temp.append(newLine);
         }
     }
 
     private void setTitle(Class<?> token, StringBuilder temp) {
-        String name = token.getSimpleName();
-        temp.append("public class ").append(name).append("Impl").
+        temp.append("public class ").append(token.getSimpleName()).append("Impl").
                 append((token.isInterface() ? " implements " : " extends ")).
-                append(name).append(" {").append(newLine).append(newLine);
+                append(token.getCanonicalName()).append(" {").append(newLine).append(newLine);
     }
 
     private void print(StringBuilder temp, Path path) throws ImplerException {
@@ -99,25 +92,32 @@ public class Implementor implements Impler {
     }
 
     private void findMethods(Class<?> token) {
-        methodSet.addAll(Arrays.asList(token.getConstructors()));
-        methodSet.addAll(Arrays.asList(token.getDeclaredMethods()));
+        for (Method method : token.getDeclaredMethods()) {
+            String title = method.getName() + Arrays.toString(method.getParameterTypes());
+            if (!methodsTitle.contains(title)) {
+                methodsTitle.add(title);
+                methodsSet.add(method);
+            }
+        }
 
         if ((token.getModifiers() & Modifier.ABSTRACT) != 0) {
             for (Class<?> c : token.getInterfaces()) {
                 findMethods(c);
             }
         }
+
+        if (token.getSuperclass() != null) {
+            findMethods(token.getSuperclass());
+        }
     }
 
     private void setMethods(Class<?> token) {
-        for (Executable m : methodSet) {
-            if ((m.getModifiers() & (Modifier.PRIVATE | Modifier.FINAL | Modifier.NATIVE)) != 0) {
+        for (Executable m : methodsSet) {
+            if ((m.getModifiers() & (Modifier.PRIVATE | Modifier.FINAL | Modifier.NATIVE | Modifier.SYNCHRONIZED)) != 0) {
                 continue;
             }
-            result.append("    @SuppressWarnings(\"unchecked\")").append(newLine);
-            result.append("    ");
-            setTitle(token, m);
 
+            setTitle(token, m);
             if (m instanceof Constructor) {
                 setConstructorBody(m);
             } else if (m instanceof Method) {
@@ -127,38 +127,34 @@ public class Implementor implements Impler {
     }
 
     private void setTitle(Class<?> token, Executable m) {
+        result.append(tab);
         if (m instanceof Constructor) {
             result.append(token.getSimpleName()).append("Impl");
         } else if (m instanceof Method) {
             setModifiers((Method) m);
-            result.append(((Method) m).getReturnType().getSimpleName()).append(" ").append(m.getName());
+            Class<?> returnType = ((Method) m).getReturnType();
+            result.append(returnType.getCanonicalName()).append(" ").append(m.getName());
         }
+
         result.append("(");
-        setArgs(m.getParameterTypes());
+        setArgs(m.getParameters());
         result.append(")");
 
         if (m.getExceptionTypes().length != 0) {
             result.append(" throws ");
             setExceptions(m.getExceptionTypes());
         }
-
     }
 
     private void setModifiers(Method m) {
         int mod = m.getModifiers();
         result.append(Modifier.toString(mod & (mod ^ Modifier.ABSTRACT) & (mod ^ Modifier.TRANSIENT))).append(" ");
-        imports.add(m.getReturnType());
     }
 
-    private void setArgs(Class<?>[] params) {
+    private void setArgs(Parameter[] params) {
         for (int i = 0; i < params.length; i++) {
-            Class<?> c = params[i];
-            imports.add(c);
-            result.append(c.getSimpleName()).append(" ");
-
-            //todo fix same names
-            result.append((char) (i + 'a'));
-
+            Parameter c = params[i];
+            result.append(c.getType().getCanonicalName()).append(" ").append(c.getName());
             if (i + 1 != params.length) {
                 result.append(", ");
             }
@@ -167,8 +163,7 @@ public class Implementor implements Impler {
 
     private void setExceptions(Class<?>[] exceptions) {
         for (int i = 0; i < exceptions.length; i++) {
-            imports.add(exceptions[i]);
-            result.append(exceptions[i].getSimpleName());
+            result.append(exceptions[i].getCanonicalName());
             if (i + 1 != exceptions.length) {
                 result.append(", ");
             }
@@ -176,19 +171,26 @@ public class Implementor implements Impler {
     }
 
     private void setConstructorBody(Executable m) {
-        result.append("{").append(newLine).append("        super(");
+        if (Modifier.isPrivate(m.getModifiers())) {
+            return;
+        }
+        containsNonPrivateConstructor = true;
+
+        result.append("{").append(newLine).append(tab).append(tab).append("super(");
+
+        Parameter[] p = m.getParameters();
         for (int i = 0; i < m.getParameterCount(); i++) {
-            result.append((char) (i + 'a'));
+            result.append(p[i].getName());
             if (i + 1 != m.getParameterCount()) {
                 result.append(", ");
             }
         }
-        result.append(");}").append(newLine);
+        result.append(");").append(newLine).append(tab).append("}").append(newLine);
     }
 
     private void setMethodBody(Class<?> c) {
         result.append(" {").append(newLine);
-        result.append("    ").append("    ");
+        result.append(tab).append(tab);
         if (c.isPrimitive()) {
             if (!c.equals(void.class)) {
                 result.append("return ");
@@ -201,7 +203,6 @@ public class Implementor implements Impler {
         } else {
             result.append("return null;");
         }
-        result.append(newLine).append("    }").append(newLine).append(newLine);
-
+        result.append(newLine).append(tab).append("}").append(newLine).append(newLine);
     }
 }
