dc3cb9af8e2d5aa5a925636eab60e1a16ac91eeb
Vitaly <tihonovcore@gmail.com>
2019-04-14 08:40:50 +0300

    change mapper to executor

--- src/ru/ifmo/rain/tihonov/crawler/WebCrawler.java
+++ src/ru/ifmo/rain/tihonov/crawler/WebCrawler.java
@@ -1,34 +1,57 @@
 package ru.ifmo.rain.tihonov.crawler;
 
 import info.kgeorgiy.java.advanced.crawler.Crawler;
-import info.kgeorgiy.java.advanced.crawler.Document;
 import info.kgeorgiy.java.advanced.crawler.Downloader;
 import info.kgeorgiy.java.advanced.crawler.Result;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
+import java.util.concurrent.*;
+
+class Counter {
+    private long started = 0;
+    private long finished = 0;
+
+    synchronized void start() {
+        started++;
+    }
+
+    synchronized void finish() {
+        finished++;
+    }
+
+    synchronized boolean working() {
+        return started != finished;
+    }
+}
 
 public class WebCrawler implements Crawler {
+    private Counter counter = new Counter();
+
     private Downloader downloader;
 
-    private Mapper loadMapper;
-    private Mapper extractMapper;
+//    private Mapper loadMapper;
+//    private Mapper extractMapper;
+
+    private ThreadPoolExecutor loader;
+    private ThreadPoolExecutor extractor;
 
     public WebCrawler(Downloader downloader, int downloaders, int extractors, int perHost) {
         this.downloader = downloader;
-        this.loadMapper = new Mapper(Integer.min(downloaders, 10)); //todo fix border
-        this.extractMapper = new Mapper(Integer.min(extractors, 10)); //todo add extractor
+//        this.loadMapper = new Mapper(Integer.min(downloaders, 10)); //todo fix border
+//        this.extractMapper = new Mapper(Integer.min(extractors, 10)); //todo add extractor
+
+        loader = new ThreadPoolExecutor(10, Integer.min(downloaders, 100), 10L, TimeUnit.SECONDS, loadQueue);
+//        extractor = new ThreadPoolExecutor(10, Integer.min(extractors, 100), 10L, TimeUnit.SECONDS, extractQueue);
     }
+
+    private BlockingQueue<Runnable> loadQueue = new ArrayBlockingQueue<>(10000);
+//    private BlockingQueue<Runnable> extractQueue = new ArrayBlockingQueue<>(10000);
 
     private final BlockingQueue<String> queue = new ArrayBlockingQueue<>(10000);
 
     @Override
     public Result download(String url, int depth) {
-//        BlockingQueue<String> loadQueue = new ArrayBlockingQueue<>(100);
 
         Map<String, Boolean> donwloaded = new ConcurrentHashMap<>();
         Map<String, IOException> exceptions = new ConcurrentHashMap<>();
@@ -40,51 +63,53 @@ public class WebCrawler implements Crawler {
 
         queue.add(url);
 
-        Function<String, Void> load = s -> {
-            List<String> list = new ArrayList<>();
-            try {
-                list = downloader.download(s).extractLinks();
-            } catch (IOException e) {
-                if (exceptions.containsKey(s)) {
-                    exceptions.get(s).addSuppressed(e);
-                } else {
-                    exceptions.put(s, e);
-                }
-            }
+        do {
+            String curr = get();
+            if (curr == null) continue;
 
-            if (distance.get(s) + 1 < depth) {
-                for (String link : list) {
-                    if (!distance.containsKey(link)) {
-                        queue.add(link);
-                        distance.put(link, distance.get(s) + 1);
+            //NOTE: if page donwloaded, but Runnable haven't finished, NPE happend
+            if (!donwloaded.containsKey(curr) /*&& distance.get(curr) != null*/ && distance.get(curr) <= depth) {
+                donwloaded.put(curr, true);
+
+                Runnable load = () -> {
+                    List<String> list = new ArrayList<>();
+                    try {
+                        list = downloader.download(curr).extractLinks();
+                    } catch (IOException e) {
+                        if (exceptions.containsKey(curr)) {
+                            exceptions.get(curr).addSuppressed(e);
+                        } else {
+                            exceptions.put(curr, e);
+                        }
                     }
-                }
+
+                    if (distance.get(curr) + 1 < depth) {
+                        for (String link : list) {
+                            if (!distance.containsKey(link)) {
+                                queue.add(link);
+                                distance.put(link, distance.get(curr) + 1);
+                            }
+                        }
+                    }
+
+                    if (!exceptions.containsKey(curr)) {
+                        synchronized (result) {
+                            result.add(curr);
+                        }
+                    }
+
+                    counter.finish();
+                };
+
+//                Runnable extract = () -> {
+//
+//                };
+
+                counter.start();
+                loader.execute(load);
             }
+        } while (!queue.isEmpty() || counter.working());
 
-            if (!exceptions.containsKey(s)) {
-                synchronized (result) {
-                    result.add(s);
-                }
-            }
-
-            return null;
-        };
-
-        boolean flag = true;
-        while (flag || loadMapper.working()) {
-            flag = false;
-            try {
-                String curr = get();
-                if (curr == null) continue;
-
-                if (!donwloaded.containsKey(curr) && distance.get(curr) <= depth) {
-                    donwloaded.put(curr, true);
-                    loadMapper.map(load, curr);
-                }
-            } catch (InterruptedException ignored) {
-            }
-
-        }
         return new Result(result, exceptions);
     }
 
@@ -92,13 +117,12 @@ public class WebCrawler implements Crawler {
         String result;
         do {
             result = queue.poll();
-        } while (result == null && (queue.size() != 0 || loadMapper.working()));
+        } while (result == null && (!queue.isEmpty() || counter.working()));
         return result;
     }
 
     @Override
     public void close() {
-        loadMapper.close();
-        extractMapper.close();
+        loader.shutdownNow();
     }
 }
