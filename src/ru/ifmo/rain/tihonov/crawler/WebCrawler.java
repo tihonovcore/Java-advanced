5566cb354aa1aa65812b5c30e29028e8f73aa28f
Vitaly <tihonovcore@gmail.com>
2019-04-14 10:52:21 +0300

    make download and extracting to different executors

--- src/ru/ifmo/rain/tihonov/crawler/WebCrawler.java
+++ src/ru/ifmo/rain/tihonov/crawler/WebCrawler.java
@@ -1,9 +1,11 @@
 package ru.ifmo.rain.tihonov.crawler;
 
 import info.kgeorgiy.java.advanced.crawler.Crawler;
+import info.kgeorgiy.java.advanced.crawler.Document;
 import info.kgeorgiy.java.advanced.crawler.Downloader;
 import info.kgeorgiy.java.advanced.crawler.Result;
 
+import javax.print.Doc;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.*;
@@ -30,23 +32,31 @@ public class WebCrawler implements Crawler {
 
     private Downloader downloader;
 
-//    private Mapper loadMapper;
-//    private Mapper extractMapper;
-
     private ThreadPoolExecutor loader;
     private ThreadPoolExecutor extractor;
 
     public WebCrawler(Downloader downloader, int downloaders, int extractors, int perHost) {
         this.downloader = downloader;
-//        this.loadMapper = new Mapper(Integer.min(downloaders, 10)); //todo fix border
-//        this.extractMapper = new Mapper(Integer.min(extractors, 10)); //todo add extractor
-
-        loader = new ThreadPoolExecutor(10, Integer.min(downloaders, 100), 10L, TimeUnit.SECONDS, loadQueue);
-//        extractor = new ThreadPoolExecutor(10, Integer.min(extractors, 100), 10L, TimeUnit.SECONDS, extractQueue);
+        int d = Integer.min(downloaders, 5000);
+        int e = Integer.min(extractors, 5000);
+        loader = new ThreadPoolExecutor(d, d, 1000L, TimeUnit.SECONDS, loadQueue);
+        extractor = new ThreadPoolExecutor(e, e, 1000L, TimeUnit.SECONDS, extractQueue);
     }
 
     private BlockingQueue<Runnable> loadQueue = new ArrayBlockingQueue<>(10000);
-//    private BlockingQueue<Runnable> extractQueue = new ArrayBlockingQueue<>(10000);
+    private BlockingQueue<Runnable> extractQueue = new ArrayBlockingQueue<>(10000);
+
+    private BlockingQueue<Pair> documents = new ArrayBlockingQueue<>(10000);
+
+    class Pair {
+        Document document;
+        String string;
+
+        Pair(Document document, String string) {
+            this.document = document;
+            this.string = string;
+        }
+    }
 
     private final BlockingQueue<String> queue = new ArrayBlockingQueue<>(10000);
 
@@ -67,49 +77,73 @@ public class WebCrawler implements Crawler {
             String curr = get();
             if (curr == null) continue;
 
-            //NOTE: if page donwloaded, but Runnable haven't finished, NPE happend
-            if (!donwloaded.containsKey(curr) /*&& distance.get(curr) != null*/ && distance.get(curr) <= depth) {
+            if (!donwloaded.containsKey(curr) && distance.get(curr) <= depth) {
                 donwloaded.put(curr, true);
 
-                Runnable load = () -> {
+                Runnable extract = () -> {
                     List<String> list = new ArrayList<>();
+
+                    Pair pair;
+                    String s = "";
                     try {
-                        list = downloader.download(curr).extractLinks();
+                        while (true) {
+                            pair = documents.poll();
+
+                            if (pair != null && pair.document != null) {
+                                s = pair.string;
+                                break;
+                            }
+                            if (loader.getActiveCount() == 0 && documents.isEmpty()) {
+                                counter.finish();
+                                return;
+                            }
+                        }
+                        list = pair.document.extractLinks();
                     } catch (IOException e) {
-                        if (exceptions.containsKey(curr)) {
-                            exceptions.get(curr).addSuppressed(e);
+                        if (exceptions.containsKey(s)) {
+                            exceptions.get(s).addSuppressed(e);
                         } else {
-                            exceptions.put(curr, e);
+                            exceptions.put(s, e);
                         }
                     }
 
-                    if (distance.get(curr) + 1 < depth) {
+                    if (distance.get(s) + 1 < depth) {
                         for (String link : list) {
                             if (!distance.containsKey(link)) {
+                                distance.put(link, distance.get(s) + 1);
                                 queue.add(link);
-                                distance.put(link, distance.get(curr) + 1);
                             }
                         }
                     }
 
-                    if (!exceptions.containsKey(curr)) {
+                    if (!exceptions.containsKey(s)) {
                         synchronized (result) {
-                            result.add(curr);
+                            result.add(s);
                         }
                     }
 
                     counter.finish();
                 };
 
-//                Runnable extract = () -> {
-//
-//                };
+                Runnable load = () -> {
+                    try {
+                        documents.add(new Pair(downloader.download(curr), curr));
+                    } catch (IOException e) {
+                        if (exceptions.containsKey(curr)) {
+                            exceptions.get(curr).addSuppressed(e);
+                        } else {
+                            exceptions.put(curr, e);
+                        }
+                        counter.finish();
+                        return;
+                    }
+                    extractor.execute(extract);
+                };
 
                 counter.start();
                 loader.execute(load);
             }
         } while (!queue.isEmpty() || counter.working());
-
         return new Result(result, exceptions);
     }
 
@@ -124,5 +158,6 @@ public class WebCrawler implements Crawler {
     @Override
     public void close() {
         loader.shutdownNow();
+        extractor.shutdownNow();
     }
 }
